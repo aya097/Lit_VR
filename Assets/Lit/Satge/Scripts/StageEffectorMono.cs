0fb3dec34f6ed1f7d4d6e63e94cbe26eee7790fc
aya097 <akase097@gmail.com>
2024-08-22 15:47:19 +0900

    add StageEffector

---
+++ Assets/Lit/Satge/Scripts/StageEffectorMono.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEffectorMono : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float LightValue;
+
+
+}
--- Assets/Lit/Satge/Test/TestCameraAnimationMono.cs
+++ Assets/Lit/Satge/Test/TestCameraAnimationMono.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class TestCameraAnimation : MonoBehaviour
+public class TestCameraAnimationMono : MonoBehaviour
 {
     void Start()
     {
